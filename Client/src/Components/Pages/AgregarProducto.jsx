38bce5b897fb16d46fa1fcd7c087fbb526c0f435
lionel <lionelprado08@gmail.com>
2025-10-28 15:40:58 -0300

    28-10

---
+++ Client/src/Components/Pages/AgregarProducto.jsx
@@ -0,0 +1,20 @@
+
+
+function AgregarProducto() {
+  return (
+    <>
+    <form action="">
+        <label htmlFor="">Codigo</label>
+        <input type="text" name="" id="" />
+        <label htmlFor="">Nombre</label>
+        <input type="text" name="" id="" />
+        <label htmlFor="">Precio</label>
+        <input type="number" name="" id="" />
+        <input type="submit" value="Registrar Producto" />
+        
+    </form>
+    </>
+  )
+}
+
+export default AgregarProducto
